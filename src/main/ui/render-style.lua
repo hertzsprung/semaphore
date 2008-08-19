70269f6e6f920b5d340e1995f0639b0e0e811879
jshaw <jshaw@torque>
2008-08-19 21:57:58 +0000

    stroke track_back as well as track_front

--- src/main/ui/render-style.lua
+++ src/main/ui/render-style.lua
@@ -10,6 +10,7 @@ require('ui.color')
 RenderStyle = {
 	canvas = Color:new{r=00, g=0.53125, b=0.26525},
 	track_back = Color.BLACK,
+	track_back_width = 0.2,
 	track_front = Color:new{r=0.53125, g=0.796875, b=0.796875},
 	track_front_width = 0.1
 }

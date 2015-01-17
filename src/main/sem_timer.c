b211243e5a8a569e3aeb05f730d6d6652ebe9690
James Shaw <js102@zepler.net>
2015-01-17 22:52:04 +0000

    get timer working with multiplier

--- src/main/sem_timer.c
+++ src/main/sem_timer.c
@@ -1,7 +1,16 @@
 #include <inttypes.h>
 
+#include "sem_error.h"
 #include "sem_timer.h"
 
-uint64_t sem_timer_now(sem_timer_context* ctx) {
-	return ctx->now;
+int sem_timer_init(sem_timer_context* ctx) {
+	return ctx->clock(&(ctx->clock_now));
+}
+
+int sem_timer_now(sem_timer_context* ctx) {
+	ctx->clock_previous = ctx->clock_now;
+	if (sem_timer_init(ctx) != SEM_OK) return SEM_ERROR;
+	uint64_t delta = ctx->clock_now - ctx->clock_previous;
+	ctx->now += delta * ctx->multiplier;
+	return SEM_OK;
 }
--- src/main/sem_timer.h
+++ src/main/sem_timer.h
@@ -4,9 +4,14 @@
 #include <inttypes.h>
 
 typedef struct {
-	uint64_t now;
+	uint64_t now; /**< current game time */
+	double multiplier; /**< how game time increases in relation to real life */
+	uint64_t clock_now; /**< current time in real life */
+	uint64_t clock_previous;
+	int (*clock)(uint64_t* now);
 } sem_timer_context;
 
-uint64_t sem_timer_now(sem_timer_context* ctx);
+int sem_timer_init(sem_timer_context* ctx);
+int sem_timer_now(sem_timer_context* ctx);
 
 #endif

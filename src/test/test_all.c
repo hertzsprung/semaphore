d44d57dec0a677800cfa83e22dd8360282aa8ac2
James Shaw <js102@zepler.net>
2015-01-17 23:04:38 +0000

    split timer tests into their own file

--- src/test/test_all.c
+++ src/test/test_all.c
@@ -3,14 +3,7 @@
 
 #include <glib.h>
 
-#include "sem_timer.h"
-#include "sem_error.h"
-
-void test_timer_increments_by_one_second(void);
-void test_timer_increments_twice_clock_with_multiple_of_two(void);
-
-int test_fixed_clock(uint64_t* now);
-int test_fixed_increment_clock(uint64_t* now);
+#include "test_timer.h"
 
 int main(int argc, char **argv) {
 	g_test_init(&argc, &argv, NULL);
@@ -19,47 +12,5 @@ int main(int argc, char **argv) {
 	g_test_add_func("/timer_increments_twice_clock_with_multiple_of_two", test_timer_increments_twice_clock_with_multiple_of_two);
 
 	return g_test_run();
-}
-
-int test_fixed_clock(uint64_t* now) {
-	*now = 9876543L;
-	return SEM_OK;
-}
-
-int test_fixed_increment_clock(uint64_t* now) {
-	static uint64_t next = 100000L;
-	next += 1000L;
-	*now = next;
-	return SEM_OK;
-}
-
-void test_timer_increments_by_one_second() {
-	uint64_t now = 123456789012345L;
-	uint64_t one_second_later = 123456789013345L;
-
-	sem_timer_context timer_ctx;
-	timer_ctx.now = now;
-	timer_ctx.multiplier = 1.0;
-	timer_ctx.clock = test_fixed_increment_clock;
-
-	sem_timer_init(&timer_ctx);
-	sem_timer_now(&timer_ctx);
-
-	g_assert_cmpuint(timer_ctx.now, ==, one_second_later);
-}
-
-void test_timer_increments_twice_clock_with_multiple_of_two() {
-	uint64_t now = 123456789012345L;
-	uint64_t two_seconds_later = 123456789014345L;
-
-	sem_timer_context timer_ctx;
-	timer_ctx.now = now;
-	timer_ctx.multiplier = 2.0;
-	timer_ctx.clock = test_fixed_increment_clock;
-
-	sem_timer_init(&timer_ctx);
-	sem_timer_now(&timer_ctx);
-
-	g_assert_cmpuint(timer_ctx.now, ==, two_seconds_later);
 }
 
---
+++ src/test/test_timer.h
@@ -0,0 +1,7 @@
+#ifndef _SEM_TEST_TIMER_H
+#define _SEM_TEST_TIMER_H
+
+void test_timer_increments_by_one_second(void);
+void test_timer_increments_twice_clock_with_multiple_of_two(void);
+
+#endif

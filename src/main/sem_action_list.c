f8ecc5f017e15eb8a9e2a4028d63ad8ec577955e
James Shaw <js102@zepler.net>
2015-01-18 21:01:55 +0000

    implement action_list execute

--- src/main/sem_action_list.c
+++ src/main/sem_action_list.c
@@ -4,9 +4,11 @@
 #include "sem_heap.h"
 
 sem_success sem_action_list_execute(sem_heap* heap, uint64_t end_time) {
-	if (heap->entries[1]->time <= end_time) {
+	while (heap->tail_idx > 1 && heap->entries[1]->time <= end_time) {
 		sem_heap_entry* entry = sem_heap_remove_earliest(heap);
-		entry->action(heap, entry->context); // TODO: check return
+		if (entry->action(heap, entry->context) != SEM_OK) {
+			return SEM_ERROR;
+		}
 	}
 	return SEM_OK;
 }
--- src/test/test_action_list.c
+++ src/test/test_action_list.c
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 #include <stdbool.h>
 #include <glib.h>
 
@@ -8,43 +9,50 @@
 #include "sem_error.h"
 #include "sem_heap.h"
 
-void test_action_list_one_old_action_executed(sem_heap* heap, const void* data);
+void test_action_list_two_old_actions_executed(sem_heap* heap, const void* data);
 void test_action_list_does_not_execute_future_action(sem_heap* heap, const void* data);
+void test_action_list_does_nothing_for_empty_list(sem_heap* heap, const void* data);
 
 int test_action(sem_heap* heap, void* context);
 
 void add_tests_action_list() {
-	add_test_heap("/action_list/one_old_action_executed", test_action_list_one_old_action_executed);
+	add_test_heap("/action_list/two_old_action_executed", test_action_list_two_old_actions_executed);
 	add_test_heap("/action_list/does_not_execute_future_action", test_action_list_does_not_execute_future_action);
+	add_test_heap("/action_list/does_nothing_for_empty_list", test_action_list_does_nothing_for_empty_list);
 }
 
 int test_action(sem_heap* heap, void* context) {
 	#pragma unused(heap)
-	*((bool*) context) = true;
+	(*((uint32_t*) context))++;
 
 	return SEM_OK;
 }
 
-void test_action_list_one_old_action_executed(sem_heap* heap, const void* data) {
+void test_action_list_two_old_actions_executed(sem_heap* heap, const void* data) {
 	#pragma unused(data)
 
-	bool action_called = false;
+	uint32_t action_called = 0;
 
-	sem_heap_entry action;
-	action.time = 1000L;
-	action.context = &action_called;
-	action.action = test_action;
+	sem_heap_entry a1, a2;
+	a1.time = 1000L;
+	a1.context = &action_called;
+	a1.action = test_action;
 
-	sem_heap_insert(heap, &action);
+	a2.time = 2000L;
+	a2.context = &action_called;
+	a2.action = test_action;
+
+	sem_heap_insert(heap, &a1);
+	sem_heap_insert(heap, &a2);
 	sem_action_list_execute(heap, 2000L); // TODO: check return
 
-	g_assert_true(action_called);
+	g_assert_cmpuint(action_called, ==, 2);
 }
 
 void test_action_list_does_not_execute_future_action(sem_heap* heap, const void* data) {
 	#pragma unused(data)
 
-	bool action_called = false;
+	uint32_t action_called = 0;
 
 	sem_heap_entry action;
 	action.time = 2050L;
@@ -54,5 +62,11 @@ void test_action_list_does_not_execute_future_action(sem_heap* heap, const void*
 	sem_heap_insert(heap, &action);
 	sem_action_list_execute(heap, 2000L); // TODO: check return
 
-	g_assert_false(action_called);
+	g_assert_cmpuint(action_called, ==, 0);
+}
+
+void test_action_list_does_nothing_for_empty_list(sem_heap* heap, const void* data) {
+	#pragma unused(data)
+
+	sem_action_list_execute(heap, 2000L); // TODO: check return
 }

459950c027a7e015b866cbf7e82f0d53fa8243bc
James Shaw <js102@zepler.net>
2015-02-21 14:33:33 +0000

    reuse track path routine, use sem_coordinate rather than (x,y) everywhere

--- src/main/sem_render.c
+++ src/main/sem_render.c
@@ -7,9 +7,9 @@
 #include "sem_world.h"
 
 void render_tiles(sem_render_context* ctx, sem_world* world);
-void render_tile(sem_render_context* ctx, uint32_t x, uint32_t y, sem_tile* tile);
-void render_tile_blank(sem_render_context* ctx, uint32_t x, uint32_t y, sem_tile* tile);
-void render_tile_track(sem_render_context* ctx, uint32_t x, uint32_t y, sem_tile* tile);
+void render_tile(sem_render_context* ctx, sem_coordinate* coord, sem_tile* tile);
+void render_tile_blank(sem_render_context* ctx, sem_coordinate* coord, sem_tile* tile);
+void render_tile_track(sem_render_context* ctx, sem_coordinate* coord, sem_tile* tile);
 void render_train(sem_render_context* ctx, sem_train* train);
 void render_track_path(sem_render_context* ctx, sem_coordinate* coord, sem_track* track);
 
@@ -21,34 +21,36 @@ void sem_render_world(sem_render_context* ctx, sem_world* world) {
 void render_tiles(sem_render_context* ctx, sem_world* world) {
 	for (uint32_t j=0; j<world->max_y; j++) {
 		for (uint32_t i=0; i<world->max_x; i++) {
-			render_tile(ctx, i, j, sem_tile_at(world, i, j));
+			sem_coordinate coord;
+			sem_coordinate_set(&coord, i, j);
+			render_tile(ctx, &coord, sem_tile_at(world, i, j));
 		}
 	}
 }
 
-// TODO: rather than passing in x and y we could use a cairo transform so that all renders happen from 0-1
+// TODO: rather than passing in coord we could use a cairo transform so that all renders happen from 0-1
 // but I'm not sure how efficient this would be, or how convenient?
-void render_tile(sem_render_context* ctx, uint32_t x, uint32_t y, sem_tile* tile) {
+void render_tile(sem_render_context* ctx, sem_coordinate* coord, sem_tile* tile) {
 	switch(tile->class) {
 	case BLANK:
-		render_tile_blank(ctx, x, y, tile);
+		render_tile_blank(ctx, coord, tile);
 		return;
 	case TRACK:
-		render_tile_track(ctx, x, y, tile);
+		render_tile_track(ctx, coord, tile);
 		return;
 	}
 }
 
-void render_tile_blank(sem_render_context* ctx, uint32_t x, uint32_t y, sem_tile* tile) {
+void render_tile_blank(sem_render_context* ctx, sem_coordinate* coord, sem_tile* tile) {
 	#pragma unused(tile)
-	cairo_rectangle(ctx->cr, x, y, 1.0, 1.0);	
+	cairo_rectangle(ctx->cr, coord->x, coord->y, 1.0, 1.0);	
+	cairo_set_line_width(ctx->cr, 0.01);
 	cairo_set_source_rgb(ctx->cr, 0.0, 0.3, 0.0);
 	cairo_stroke(ctx->cr);
 }
 
-void render_tile_track(sem_render_context* ctx, uint32_t x, uint32_t y, sem_tile* tile) {
-	cairo_move_to(ctx->cr, x + 0.5 + SEM_COMPASS_X(tile->track->start)/2.0, y + 0.5 + SEM_COMPASS_Y(tile->track->start)/2.0);
-	cairo_line_to(ctx->cr, x + 0.5 + SEM_COMPASS_X(tile->track->end)/2.0, y + 0.5 + SEM_COMPASS_Y(tile->track->end)/2.0);
+void render_tile_track(sem_render_context* ctx, sem_coordinate* coord, sem_tile* tile) {
+	render_track_path(ctx, coord, tile->track);
 	cairo_set_source_rgb(ctx->cr, 0.0, 0.0, 0.0);
 	cairo_set_line_width(ctx->cr, 0.2);
 	cairo_stroke_preserve(ctx->cr);

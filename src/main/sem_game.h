9b2418d2246155811d24f3a8eacf63f771f5bced
James Shaw <js102@zepler.net>
2015-08-31 10:10:03 +0100

    introduce sem_game

---
+++ src/main/sem_game.h
@@ -0,0 +1,10 @@
+#ifndef SEM_GAME_H
+#define SEM_GAME_H
+
+#include "sem_world.h"
+
+typedef struct {
+	sem_world world;
+} sem_game;
+
+#endif
--- src/test/test_portal.c
+++ src/test/test_portal.c
@@ -3,18 +3,20 @@
 
 #include "test_portal.h"
 
+#include "sem_game.h"
 #include "sem_heap.h"
 #include "sem_input.h"
 #include "sem_portal.h"
 #include "sem_world.h"
 
 typedef struct {
-	sem_world world;
+	sem_game game;
 	sem_track* track;
 } test_portal_context;
 
 void test_portal_train_enters_from_portal(test_portal_context* test_ctx, const void* data);
 void test_portal_train_exits_into_portal(test_portal_context* test_ctx, const void* data);
+void test_portal_earn_revenue_when_train_exits_into_portal(test_portal_context* test_ctx, const void* data);
 
 void add_test_portal(const char *test_name, void (*test)(test_portal_context*, const void* data));
 void test_portal_setup(test_portal_context* test_ctx, const void* data);
@@ -23,6 +25,7 @@ void test_portal_teardown(test_portal_context* test_ctx, const void* data);
 void add_tests_portal() {
 	add_test_portal("/portal/train_enters_from_portal", test_portal_train_enters_from_portal);
 	add_test_portal("/portal/train_exits_into_portal", test_portal_train_exits_into_portal);
+	add_test_portal("/portal/earn_revenue_when_train_exits_into_portal", test_portal_earn_revenue_when_train_exits_into_portal);
 }
 
 void add_test_portal(const char *test_name, void (*test)(test_portal_context*, const void* data)) {
@@ -32,29 +35,29 @@ void add_test_portal(const char *test_name, void (*test)(test_portal_context*, c
 void test_portal_setup(test_portal_context* test_ctx, const void* data) {
 	#pragma unused(data)
 
-	test_ctx->world.max_x = 4;
-	test_ctx->world.max_y = 4;
-	sem_world_init_blank(&(test_ctx->world));
+	test_ctx->game.world.max_x = 4;
+	test_ctx->game.world.max_y = 4;
+	sem_world_init_blank(&(test_ctx->game.world));
 
 	test_ctx->track = malloc(sizeof(sem_track));
 	sem_track_set(test_ctx->track, SEM_WEST, SEM_EAST);
 
-	sem_tile_set_entry(sem_tile_at(&(test_ctx->world), 0, 0), test_ctx->track);
-	sem_tile_set_track(sem_tile_at(&(test_ctx->world), 1, 0), test_ctx->track);
-	sem_tile_set_track(sem_tile_at(&(test_ctx->world), 2, 0), test_ctx->track);
-	sem_tile_set_exit(sem_tile_at(&(test_ctx->world), 3, 0), test_ctx->track);
+	sem_tile_set_entry(sem_tile_at(&(test_ctx->game.world), 0, 0), test_ctx->track);
+	sem_tile_set_track(sem_tile_at(&(test_ctx->game.world), 1, 0), test_ctx->track);
+	sem_tile_set_track(sem_tile_at(&(test_ctx->game.world), 2, 0), test_ctx->track);
+	sem_tile_set_exit(sem_tile_at(&(test_ctx->game.world), 3, 0), test_ctx->track);
 }
 
 void test_portal_teardown(test_portal_context* test_ctx, const void* data) {
 	#pragma unused(data)
 
-	sem_world_destroy(&(test_ctx->world));
+	sem_world_destroy(&(test_ctx->game.world));
 	free(test_ctx->track);
 }
 
 void test_portal_train_enters_from_portal(test_portal_context* test_ctx, const void* data) {
 	#pragma unused(data)
-	sem_world* world = &(test_ctx->world);
+	sem_world* world = &(test_ctx->game.world);
 
 	sem_train_entry_context context;
 	context.world = world;
@@ -90,7 +93,7 @@ void test_portal_train_enters_from_portal(test_portal_context* test_ctx, const v
 
 void test_portal_train_exits_into_portal(test_portal_context* test_ctx, const void* data) {
 	#pragma unused(data)
-	sem_world* world = &(test_ctx->world);
+	sem_world* world = &(test_ctx->game.world);
 
 	sem_train* train = malloc(sizeof(sem_train));	
 	sem_train_init(train);
@@ -130,4 +133,33 @@ void test_portal_train_exits_into_portal(test_portal_context* test_ctx, const vo
 	g_assert_true(move_action->function(world->actions, move_action) == SEM_OK);
 
 	g_assert_cmpuint(world->trains->tail_idx, ==, 0);
+}
+
+void test_portal_earn_revenue_when_train_exits_into_portal(test_portal_context* test_ctx, const void* data) {
+	#pragma unused(data)
+	sem_world* world = &(test_ctx->game.world);
+
+	sem_train* train = malloc(sizeof(sem_train));	
+	sem_train_init(train);
+	train->state = MOVING;
+	train->direction = SEM_EAST;
+	sem_world_add_train(world, train);
+
+	sem_car* head_car = malloc(sizeof(sem_car));
+	sem_coordinate_set(&(head_car->position), 2, 0);
+	head_car->track = sem_tile_at(world, 2, 0)->track;
+
+	sem_train_add_car(train, head_car);
+
+	sem_action action;
+	action.time = 2000;
+	action.context = train;
+	action.function = sem_move_train_action;
+	action.dynamically_allocated = false;
+
+	g_assert_true(sem_move_train_action(world->actions, &action) == SEM_OK);
+
+	sem_action* move_action = sem_heap_remove_earliest(world->actions);
+	g_assert_nonnull(move_action);
+	g_assert_true(move_action->function(world->actions, move_action) == SEM_OK);
 }
